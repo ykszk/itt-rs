142be7fd59ac13239124f7b42145ad0d097a98e2
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2022-07-15 00:33:45 +0900

    Implement "/list"

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,59 @@ use tera::Tera;
 #[macro_use]
 extern crate lazy_static;
 use std::net::{IpAddr, Ipv4Addr};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+
+struct DBItem {
+    image_name: String,
+    image_path: PathBuf,
+    tag_path: PathBuf,
+    checked_tags: Vec<String>,
+}
+
+impl DBItem {
+    pub fn new(image_path: PathBuf) -> Self {
+        let image_name = image_path.to_str().unwrap().into();
+        let image_path = Path::new("/images").join(image_path.file_name().unwrap());
+        let mut tag_path = image_path.clone();
+        tag_path.set_extension("txt");
+        let checked_tags = Vec::new(); // TODO: load tags from existing file.
+        DBItem {
+            image_name,
+            image_path,
+            tag_path,
+            checked_tags,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct TxtDB {
+    items: Vec<DBItem>,
+}
+
+impl TxtDB {
+    pub fn new(img_dir: &Path) -> Result<Self, Box<dyn std::error::Error>> {
+        let re = regex::Regex::new(r"^.+\.jpg|png|jpeg$").unwrap();
+        let items: Vec<_> = std::fs::read_dir(img_dir)?
+            .into_iter()
+            .filter_map(|entry| entry.ok())
+            .filter(|path| re.is_match(path.file_name().to_str().unwrap()))
+            .map(|img_path| DBItem::new(img_path.path()))
+            .collect();
+        Ok(TxtDB { items })
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct ToolState {
+    config: ToolConfig,
+    db: TxtDB,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(crate = "rocket::serde")]
@@ -43,12 +95,19 @@ struct Args {
 }
 
 #[get("/")]
-fn index(state: &State<ToolConfig>) -> RawHtml<String> {
-    // "Hello, world!"
-    // format!("Hello world\n{:?}", state.tags)
+fn index(state: &State<ToolState>) -> RawHtml<String> {
     let mut context = tera::Context::new();
-    context.insert("tags", &state.tags);
+    context.insert("tags", &state.config.tags);
     RawHtml(TEMPLATES.render("index.html", &context).unwrap())
+}
+
+#[get("/list")]
+fn list(state: &State<ToolState>) -> RawHtml<String> {
+    let mut context = tera::Context::new();
+    context.insert("tags", &state.config.tags);
+    context.insert("multilabel", &state.config.multilabel);
+    context.insert("image_name_path_tags", &state.db.items);
+    RawHtml(TEMPLATES.render("list.html", &context).unwrap())
 }
 
 lazy_static! {
@@ -77,7 +136,7 @@ lazy_static! {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let mut rocket_config = rocket::Config::default();
-    let config: ToolConfig = Figment::new().merge(Toml::file(args.config)).extract()?;
+    let mut config: ToolConfig = Figment::new().merge(Toml::file(&args.config)).extract()?;
     rocket_config.port = config.server.port.clone();
     let ip_addr = match config.server.host.as_str() {
         "localhost" => IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
@@ -102,14 +161,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         ])?;
     }
 
+    // if config.img_dir.is_relative() {
+    //     config.img_dir = args.config.parent().unwrap().join(config.img_dir.as_path());
+    // };
+    println!("{:?}", config.img_dir);
+    let db = TxtDB::new(&config.img_dir.as_path())?;
+    let fs = rocket::fs::FileServer::from(config.img_dir.as_path());
+    let state = ToolState { config, db };
     if args.open {
         let url = format!("http://{}:{}", rocket_config.address, rocket_config.port);
         webbrowser::open(&url)?;
     }
     let r = rocket::custom(rocket_config)
-        .mount("/", routes![index])
-        // .attach(Template::fairing())
-        .manage(config);
+        .mount("/", routes![index, list])
+        .mount("/images", fs)
+        .manage(state);
     let _ = r.launch().await;
     Ok(())
 }
