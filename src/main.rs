7717f61fe1e7c5113341f5b981459fdd5c90fcc4
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2022-07-15 17:27:02 +0900

    WIP: implement put

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use rocket::figment::{
     providers::{Format, Toml},
     Figment,
 };
-use rocket::response::content::RawHtml;
+use rocket::response::content::{self, RawHtml};
 use rocket::serde::{Deserialize, Serialize};
 use rocket::State;
 use tera::Tera;
@@ -25,12 +25,21 @@ struct DBItem {
 }
 
 impl DBItem {
-    pub fn new(image_path: PathBuf) -> Self {
+    fn load_tags(tag_path: &Path) -> Vec<String> {
+        let contents = std::fs::read_to_string(tag_path);
+        if let Ok(contents) = contents {
+            contents.lines().map(|s| s.into()).collect()
+        } else {
+            Vec::new()
+        }
+    }
+    pub fn new(image_path: PathBuf, tag_dir: &Path) -> Self {
         let image_name = image_path.to_str().unwrap().into();
-        let image_path = Path::new("/images").join(image_path.file_name().unwrap());
-        let mut tag_path = image_path.clone();
+        let mut tag_path = tag_dir.join(image_path.file_name().unwrap());
         tag_path.set_extension("txt");
-        let checked_tags = Vec::new(); // TODO: load tags from existing file.
+        let image_path = Path::new("/images").join(image_path.file_name().unwrap());
+        let checked_tags = Self::load_tags(tag_path.as_path());
+        // println!("{:?}: {:?}", tag_path, checked_tags);
         DBItem {
             image_name,
             image_path,
@@ -47,13 +56,13 @@ struct TxtDB {
 }
 
 impl TxtDB {
-    pub fn new(img_dir: &Path) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn new(img_dir: &Path, tag_dir: &Path) -> Result<Self, Box<dyn std::error::Error>> {
         let re = regex::Regex::new(r"^.+\.jpg|png|jpeg$").unwrap();
         let items: Vec<_> = std::fs::read_dir(img_dir)?
             .into_iter()
             .filter_map(|entry| entry.ok())
             .filter(|path| re.is_match(path.file_name().to_str().unwrap()))
-            .map(|img_path| DBItem::new(img_path.path()))
+            .map(|img_path| DBItem::new(img_path.path(), tag_dir))
             .collect();
         Ok(TxtDB { items })
     }
@@ -110,6 +119,32 @@ fn list(state: &State<ToolState>) -> RawHtml<String> {
     RawHtml(TEMPLATES.render("list.html", &context).unwrap())
 }
 
+// use rocket::form::Form;
+use rocket::form::{Form, Strict};
+use rocket::http::RawStr;
+#[derive(FromForm, Debug)]
+struct MyForm {
+    numbers: Vec<(String, Boolean)>,
+}
+#[put("/put", data = "<data>")]
+fn put(state: &State<ToolState>, data: Form<MyForm>) -> String {
+    println!("{:?}", data);
+    let tags = if state.config.multilabel { "" } else { "" };
+    "".into()
+}
+use rocket::http::ContentType;
+#[get("/static/main.js")]
+fn mainjs() -> (ContentType, &'static str) {
+    let s = include_str!("../python/static/main.js");
+    (ContentType::JavaScript, s)
+}
+
+#[get("/static/style.css")]
+fn stylecss() -> (ContentType, &'static str) {
+    let s = include_str!("../python/static/style.css");
+    (ContentType::CSS, s)
+}
+
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
         let mut tera = tera::Tera::new("/dev/null/*").unwrap();
@@ -136,11 +171,11 @@ lazy_static! {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let mut rocket_config = rocket::Config::default();
-    let mut config: ToolConfig = Figment::new().merge(Toml::file(&args.config)).extract()?;
-    rocket_config.port = config.server.port.clone();
+    let config: ToolConfig = Figment::new().merge(Toml::file(&args.config)).extract()?;
+    rocket_config.port = config.server.port;
     let ip_addr = match config.server.host.as_str() {
         "localhost" => IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-        octets @ _ => octets.parse()?,
+        octets => octets.parse()?,
     };
     rocket_config.address = ip_addr;
     {
@@ -165,7 +200,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //     config.img_dir = args.config.parent().unwrap().join(config.img_dir.as_path());
     // };
     println!("{:?}", config.img_dir);
-    let db = TxtDB::new(&config.img_dir.as_path())?;
+    let db = TxtDB::new(config.img_dir.as_path(), config.tag_dir.as_path())?;
     let fs = rocket::fs::FileServer::from(config.img_dir.as_path());
     let state = ToolState { config, db };
     if args.open {
@@ -173,7 +208,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         webbrowser::open(&url)?;
     }
     let r = rocket::custom(rocket_config)
-        .mount("/", routes![index, list])
+        .mount("/", routes![index, list, put, mainjs, stylecss])
         .mount("/images", fs)
         .manage(state);
     let _ = r.launch().await;
